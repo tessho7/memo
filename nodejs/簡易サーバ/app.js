ae07ff7f5b4c787ab0383f6815c65641ae9748d2
tessho7 <ttamsg@gmail.com>
2013-12-26 14:43:46 +0900

    Update app.js
    ・ポート指定できるようにした
    ・ドキュメントルート設定時、最後に「/」が付いていても動くよう変更
    ・ファイルにパラメータやハッシュ(? or & or #)がある場合を考慮
    　・urlをrequireし、pathnameを取得
    ・サーバ起動時、指定されたポートを表示

--- nodejs/簡易サーバ/app.js
+++ nodejs/簡易サーバ/app.js
@@ -1,9 +1,12 @@
-var http	= require('http'),
-	fs		= require('fs')
+var http    = require('http'),
+    fs      = require('fs'),
+    url     = require('url')
 ;
 
 // ドキュメントルート設定
 var htdocs = process.argv[2] || __dirname;
+htdocs = htdocs.replace(/(\/$)/g, '');
+var port = process.argv[3] || 3001;
 
 // mime types 読込み／検索用データ作成
 var mimeTypes = require(__dirname + '/mimeTypes');
@@ -14,35 +17,38 @@ console.log(mimeTypesRegexp);
 
 // エラー時のレスポンス
 var pageError = function(res){
-	res.writeHead(404)
-	res.end(http.STATUS_CODES[404]);
+    res.writeHead(404)
+    res.end(http.STATUS_CODES[404]);
 }
 
 // サーバー
 var server = http.createServer(function(req,res){
-	if(req.url.match('favicon.ico')){
-		res.end('');
-	}else{
-		if( /^\/$/.test(req.url) ){
-			req.url = '/index.html';
-		}
-		var path = htdocs + req.url;
-		fs.stat(path, function(err, fileInfo){
-			if(err){
-				console.error(err)
-				pageError(res);
-			}else{
-				var extc = path.match(mimeTypesRegexp)[0];
-				res.writeHead(200, {
-					'Content-Length': fileInfo.size,
-					'Content-Type'	: mimeTypes[extc]
-				});
-				var frs = fs.createReadStream( path )
-					.on('error', function(err){ console.error(err); })
-				;
-				frs.pipe(res);
-			}
-		});
-	}
+    if(req.url.match('favicon.ico')){
+        res.end('');
+    }else{
+        if( /^\/$/.test(req.url) ){
+            req.url = '/index.html';
+        }
+        var urlParse = url.parse(req.url);
+        var path = htdocs + urlParse.pathname;
+        fs.stat(path, function(err, fileInfo){
+            if(err){
+                console.error(err)
+                pageError(res);
+            }else{
+                var extc = path.match(mimeTypesRegexp)[0];
+                res.writeHead(200, {
+                    'Content-Length': fileInfo.size,
+                    'Content-Type'  : mimeTypes[extc]
+                });
+                var frs = fs.createReadStream( path )
+                    .on('error', function(err){ console.error(err); })
+                ;
+                frs.pipe(res);
+            }
+        });
+    }
 });
-server.listen(3001);
+server.listen(port, null, null, function(){
+    console.log('Start Server(port=' + port + ')');
+});
