5d8a5a1b599724bdfd01c8e3846a334cbb47a25e
tessho <ttamsg@gmail.com>
2018-03-18 20:18:15 +0900

    hulu プレイヤー情報
    - 数値エラー処理追加

--- bookmarklet/hulu-playerInfo.js
+++ bookmarklet/hulu-playerInfo.js
@@ -1,21 +1,7 @@
 // 作りかけ
 (function huluInfo() {
-	const id = 'playZero'
-	document.getElementById(id) && document.getElementById(id).remove()
-	const div = document.createElement("div")
-	document.body.appendChild(div)
-	div.id = id
-	div.style.position = 'fixed'
-	div.style.right = div.style.bottom = 0
-	div.style.backgroundColor = 'rgba(128, 128, 128, 0.5)'
-	div.style.color = 'white'
-	div.style.fontSize = '10px'
-	div.style.padding = '.2em'
-	div.style.border = 'solid white 1px'
-	div.innerHTML = ''
-
 	// 秒→分、秒
-	const splitSeconds = function(second){
+	const splitSeconds = function(second = 0){
 		return {
 			hour: Math.floor(second / 3600),
 			min: Math.floor(second / 60) % 60,
@@ -29,15 +15,31 @@
 			('00' + splitSeconds.sec).slice(-2)
 		].join(':')
 	}
-
 	// 再生位置を表示
-	const player = document.getElementById('player_html5_api')
 	const display = function(){
 		const current = printSeconds(splitSeconds(player.currentTime))
 		const duration = printSeconds(splitSeconds(player.duration))
 		const percent = ((player.currentTime / player.duration) * 100).toFixed(0)
 		div.innerHTML = `${current} / ${duration}(${percent}%)`
 	}
+	// 定義
+	const id = 'playZero'
+	const player = document.getElementById('player_html5_api')
+	// 初期化
 	player.removeEventListener('timeupdate', display, false)
+	document.getElementById(id) && document.getElementById(id).remove()
+	// 描画
+	const div = document.createElement('div')
+	document.body.appendChild(div)
+	div.id = id
+	div.style.position = 'fixed'
+	div.style.right = div.style.bottom = 0
+	div.style.backgroundColor = 'rgba(128, 128, 128, 0.5)'
+	div.style.color = 'white'
+	div.style.fontSize = '10px'
+	div.style.padding = '.2em'
+	div.style.border = 'solid white 1px'
+	div.innerHTML = ''
+	// イベント
 	player.addEventListener('timeupdate', display, false)
 })()
