f659dd699581244a14a3f9d3061cedbfd2d656e0
tessho <ttamsg@gmail.com>
2018-03-18 16:35:44 +0900

    hulu プレイヤー情報
    - 微妙に使いづらいのでなにか作ろうかと

---
+++ bookmarklet/hulu-playerInfo.js
@@ -0,0 +1,41 @@
+// 作りかけ
+function huluInfo() {
+	const id = 'playZero'
+	document.getElementById(id) && document.getElementById(id).remove()
+	const div = document.createElement("div")
+	document.body.appendChild(div)
+	div.id = id
+	div.style.position = 'fixed'
+	div.style.right = div.style.bottom = 0
+	div.style.backgroundColor = 'rgba(128, 128, 128, 0.5)'
+	div.style.color = 'white'
+	div.style.padding = '.2em'
+	div.style.border = 'solid white 1px'
+	div.innerHTML = ''
+	
+	// 秒→分、秒
+	const splitSeconds = function(second){
+		return {
+			hour: Math.floor(second / 3600),
+			min: Math.floor(second / 60) % 60,
+			sec: Math.floor((second % 60) % 60)
+		}
+	}
+	const printSeconds = function(splitSeconds){
+		return [
+			('00' + splitSeconds.hour).slice(-2),
+			('00' + splitSeconds.min).slice(-2),
+			('00' + splitSeconds.sec).slice(-2)
+		].join(':')
+	}
+	
+	// 再生位置を表示
+	const player = document.getElementById('player_html5_api')
+	const display = function(){
+		const current = printSeconds(splitSeconds(player.currentTime))
+		const duration = printSeconds(splitSeconds(player.duration))
+		div.innerHTML = `${current} / ${duration}`
+	}
+	player.removeEventListener('timeupdate', display, false)
+	player.addEventListener('timeupdate', display, false)
+}
